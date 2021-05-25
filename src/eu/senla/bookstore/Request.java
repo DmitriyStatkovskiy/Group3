ae94c825bca09dce76e8e32339b33dd82ba6c902
Faceroll17 <59966721+Dmitry.St@users.noreply.github.com>
2021-05-25 17:31:03 +0300

    added RequestService class

--- src/eu/senla/bookstore/Request.java
+++ src/eu/senla/bookstore/Request.java
@@ -1,11 +1,12 @@
 package eu.senla.bookstore;
 
+import java.util.Objects;
+
 public class Request {
-    private String requestStatus;
+    private String requestStatus = "new";
     private String requestedBookName;
 
-    public Request(String requestStatus, String requestedBookName) {
-        this.requestStatus = requestStatus;
+    public Request(String requestedBookName) {
         this.requestedBookName = requestedBookName;
     }
 
@@ -28,4 +29,17 @@ public class Request {
         this.requestedBookName = requestedBookName;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Request request = (Request) o;
+        return Objects.equals(getRequestStatus(), request.getRequestStatus()) &&
+                Objects.equals(getRequestedBookName(), request.getRequestedBookName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getRequestStatus(), getRequestedBookName());
+    }
 }
--- src/eu/senla/bookstore/RequestService.java
+++ src/eu/senla/bookstore/RequestService.java
@@ -5,8 +5,16 @@ import java.util.ArrayList;
 public class RequestService {
     ArrayList<Request> requests = new ArrayList<>();
 
-    public void addRequest(String requestedBookName){
-        requests.add(new Request("new",requestedBookName));
+    public void addRequest(String requestedBookName) {
+        requests.add(new Request(requestedBookName));
     }
 
+    public void closeRequest(String bookName) {
+        for (Request request : requests
+        ) {
+            if (request.getRequestedBookName().equals(bookName)) {
+                request.setRequestStatus("completed");
+            }
+        }
+    }
 }
