d1cf4db0450935aa83e57dc37683abec5fadafc1
Faceroll17 <59966721+Dmitry.St@users.noreply.github.com>
2021-05-25 17:33:01 +0300

    added RequestService class

--- src/eu/senla/bookstore/OrderService.java
+++ src/eu/senla/bookstore/OrderService.java
@@ -20,7 +20,7 @@ public class OrderService {
             }
             if (orderNumber.equals(order.getOrderNumber())&&status.equals("canceled")) {
                 order.setOrderStatus(status);
-                order.setOrderFulfillmentDate(LocalDate.of(1970,1,1));
+                order.setOrderFulfillmentDate(LocalDate.of(1970,1,1)); //used in orderToString
             }
 
         }
