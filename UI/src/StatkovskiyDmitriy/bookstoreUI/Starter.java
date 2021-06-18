525b6641c58381c5fa20559fb570e7469ff85b80
Faceroll17 <59966721+Dmitry.St@users.noreply.github.com>
2021-06-18 15:27:21 +0300

    configure logger

--- UI/src/StatkovskiyDmitriy/bookstoreUI/Starter.java
+++ UI/src/StatkovskiyDmitriy/bookstoreUI/Starter.java
@@ -1,7 +1,7 @@
 package StatkovskiyDmitriy.bookstoreUI;
 
 import StatkovskiyDmitriy.bookstoreUI.menu.MenuController;
-import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.PropertyConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,9 +10,10 @@ public class Starter {
     static Logger logger = LoggerFactory.getLogger(Starter.class);
 
     public static void main(String[] args) {
-        BasicConfigurator.configure();
-        logger.info("Test log");
-        logger.warn("Tratata");
+        PropertyConfigurator.configure("log4j.properties");
+        logger.info("Info: Test log");
+        logger.warn("Warning: PewPew");
+        logger.error("Error: Tratata");
         MenuController.getInstance().run();
     }
 }
