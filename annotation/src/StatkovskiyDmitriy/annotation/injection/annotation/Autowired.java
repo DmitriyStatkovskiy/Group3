55806bd75648503a521dbf3ec48c1f6747b98e2b
dsfaceroll_laptop <frnx.set@gmail.com>
2021-07-07 21:27:15 +0300

    init commit

--- annotation/src/StatkovskiyDmitriy/annotation/injection/annotation/Autowired.java
+++ annotation/src/StatkovskiyDmitriy/annotation/injection/annotation/Autowired.java
@@ -1,15 +1,11 @@
-package StatkovskiyDmitriy.annotation;
+package StatkovskiyDmitriy.annotation.injection.annotation;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface ConfigProperty {
-    public String configName();
-
-    public String propertyName();
-
+@Target(ElementType.FIELD)
+public @interface Autowired {
 }
