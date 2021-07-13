f7b2a9401b34eef08d27024a4216e03212de2553
Faceroll17 <59966721+Dmitry.St@users.noreply.github.com>
2021-07-13 17:41:38 +0300

    added some changes

--- annotation/src/StatkovskiyDmitriy/annotation/ObjectFactory.java
+++ annotation/src/StatkovskiyDmitriy/annotation/ObjectFactory.java
@@ -1,19 +1,21 @@
 package StatkovskiyDmitriy.annotation;
 
+import StatkovskiyDmitriy.bookstoreUI.menu.MenuController;
+
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ObjectFactory {
     private static ObjectFactory ourInstance = new ObjectFactory();
-    private Config config ;
+    private Config config;
 
     public static ObjectFactory getOurInstance() {
         return ourInstance;
     }
 
     public ObjectFactory() {
-        new JavaConfig("DmitriyStatkovskiy",)
+        config = new JavaConfig("DmitriyStatkovskiy", new HashMap<>(Map.of(MenuController.class, MenuController.class)));
     }
 
     public <T> T createObject(Class<T> type) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
