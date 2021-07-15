fd966ea6f204f82bca6d47294cac3e926925cc65
Faceroll17 <59966721+Dmitry.St@users.noreply.github.com>
2021-07-15 15:45:48 +0300

    minor changes

--- annotation/src/StatkovskiyDmitriy/annotation/injection/ObjectFactoryT.java
+++ annotation/src/StatkovskiyDmitriy/annotation/injection/ObjectFactoryT.java
@@ -2,10 +2,14 @@ package StatkovskiyDmitriy.annotation.injection;
 
 import StatkovskiyDmitriy.annotation.injection.exception.InjectionException;
 
+import java.lang.reflect.Field;
+
+
 public class ObjectFactoryT {
     public <T> T createBean(Class<T> aClass) {
         try {
 
+            aClass.getDeclaredConstructor().setAccessible(true);
             return aClass.getDeclaredConstructor().newInstance();
         } catch (ReflectiveOperationException e) {
             throw new InjectionException("Bean creation failed", e);
