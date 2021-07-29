8bd957acadfb54140449cf8c8f4cf9468d29cf39
Faceroll17 <59966721+Dmitry.St@users.noreply.github.com>
2021-07-29 16:34:42 +0300

    part 2 in progress

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -1,8 +1,60 @@
-INSERT INTO pc VALUES
-    (1,1,2300,256,80,'x12',700),
-    (2,2,3300,512,200,'x24',799),
-    (3,3,4300,1024,200,'x48',1700),
-    (4,4,333,128,60,'x12',300),
-    (5,13,433,512,500,'x64',899),
-    (6,14,2000,512,200,'x24',999)
+/*task1*/
+SELECT model, speed, hd
+FROM pc
+WHERE price < '500';
+
+/*task2*/
+SELECT maker
+FROM product
+WHERE type = 'printer';
+
+/*task3*/
+SELECT model, ram, screen
+FROM laptop
+WHERE price > '1000';
+
+/*task4*/
+SELECT *
+FROM printer
+where color = 'y';
+
+/*task5*/
+SELECT model, speed, hd
+FROM pc
+WHERE (cd = 'x12' OR cd = 'x24')
+  AND price < '600';
+
+/*task6*/
+SELECT maker, speed
+FROM product
+         INNER JOIN laptop l on product.model = l.model
+where hd > 300;
+
+/*task7*/
+SELECT p.model, price FROM product p, laptop l WHERE
+        p.model = l.model AND p.maker = 'AAA'
+UNION
+SELECT p.model, price FROM product p, pc c WHERE
+        p.model = c.model AND p.maker = 'AAA'
+UNION
+SELECT p.model, price FROM product p, printer r WHERE
+        p.model = r.model AND p.maker = 'AAA';
+/*task8*/
+/*task9*/
+/*task10*/
+/*task11*/
+/*task12*/
+/*task13*/
+/*task14*/
+/*task15*/
+/*task16*/
+/*task17*/
+/*task18*/
+/*task19*/
+/*task20*/
+/*task21*/
+/*task22*/
+/*task23*/
+/*task24*/
+/*task25*/
 
