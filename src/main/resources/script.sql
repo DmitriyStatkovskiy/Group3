7f2f78ae7bfcad6427a725603d73cf992a965b91
Faceroll17 <59966721+Dmitry.St@users.noreply.github.com>
2021-07-30 11:43:11 +0300

    part 2 in progress

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -56,13 +56,40 @@ WHERE type = 'pc' EXCEPT
 SELECT DISTINCT maker
 FROM product
 Where type = 'laptop';
+
 /*task9*/
+SELECT DISTINCT maker
+FROM pc
+         INNER JOIN product ON pc.model = product.model
+WHERE pc.speed >= 450;
+
 /*task10*/
+SELECT model, price
+FROM printer
+WHERE price = (SELECT MAX(price) FROM printer);
+
 /*task11*/
+SELECT AVG(speed) FROM pc;
+
 /*task12*/
+SELECT AVG(speed)
+FROM laptop
+WHERE price > '1000';
+
 /*task13*/
+SELECT AVG(pc.speed)
+FROM pc, product
+WHERE pc.model = product.model AND product.maker = 'AAA';
+
 /*task14*/
+SELECT speed, sum(price)/count(price) AS avg_price
+FROM pc
+GROUP BY speed;
+
+
 /*task15*/
+SELECT hd FROM pc GROUP BY (hd) HAVING COUNT(model) >= 2;
+
 /*task16*/
 /*task17*/
 /*task18*/
