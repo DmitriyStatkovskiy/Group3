2eb590e84e2d6cb2926545e44a4f739f8034f370
Faceroll17 <59966721+Dmitry.St@users.noreply.github.com>
2021-08-03 11:03:18 +0300

    added bookstore db

--- src/main/resources/bookstore.sql
+++ src/main/resources/bookstore.sql
@@ -1,41 +1,37 @@
 CREATE TABLE IF NOT EXISTS books
 (
-    id          serial PRIMARY KEY,
+    id          serial NOT NULL PRIMARY KEY,
     status      varchar(50),
     name        varchar(50),
     edition     varchar(50),
     price       money,
     description varchar(255),
     isOld       BOOLEAN,
-    received    timestamp DEFAULT now()
+    received    timestamp DEFAULT now(),
+    order_id    integer
 );
 
 CREATE TABLE IF NOT EXISTS orders
 (
-    id           varchar(50) NOT NULL PRIMARY KEY,
-    customerName varchar(50),
-    status       varchar(50),
-    created      timestamp DEFAULT now(),
-    closed       timestamp,
-    price        money
+    id            serial NOT NULL PRIMARY KEY,
+    customer_name varchar(50),
+    status        varchar(50),
+    created       timestamp DEFAULT now(),
+    closed        timestamp,
+    price         money
 );
 
 CREATE TABLE IF NOT EXISTS requests
 (
-    id       varchar(50) NOT NULL PRIMARY KEY,
+    id       serial NOT NULL PRIMARY KEY,
     status   varchar(50),
     quantity smallint,
     book_id  integer
-);
-
-CREATE TABLE IF NOT EXISTS book_order
-(
-    id       serial PRIMARY KEY,
-    book_id  integer,
-    order_id varchar(50)
 );
 
 ALTER TABLE requests
     ADD FOREIGN KEY (book_id) REFERENCES books (id);
 
 
+ALTER TABLE books
+    ADD FOREIGN KEY (order_id) REFERENCES orders (id);
